8e111faabae89b29a4890d80d07cd49476398ea4
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2024-08-08 11:42:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,11 @@ if st.button('생성하기'):
                         const dx = e.clientX - fixedHandleX;
                         const dy = e.clientY - fixedHandleY;
                         const angle = Math.atan2(dy, dx) * (180 / Math.PI);
+                        if (handle.id.endsWith('a')) {{
+                            bar.style.transformOrigin = 'left';
+                        }} else {{
+                            bar.style.transformOrigin = 'right';
+                        }}
                         bar.style.transform = `rotate(${{angle}}deg)`;
                         bar.setAttribute('data-angle', angle);
                     }}
